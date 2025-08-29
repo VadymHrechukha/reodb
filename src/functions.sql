abd7f2e8dfa039c00e06c7bb300bf3a248c6f2c2
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2025-08-29 14:16:31 +0300

    HP-2663 Optimize payload compression and notification handling

--- src/functions.sql
+++ src/functions.sql
@@ -2446,13 +2446,13 @@ BEGIN
 
     payload_text := payload::TEXT;
     IF octet_length(payload_text) > 8000 THEN
-        SELECT gzip_compress(payload_text) INTO compressed;
-        IF length(compressed) > 8000 THEN
-            RAISE NOTICE 'Skipping notification for % because compressed payload is too large: % bytes', a_table_name, length(compressed);
+        SELECT encode(gzip_compress(payload_text), 'base64') INTO compressed;
+        IF octet_length(compressed::text) >= 8000 THEN
+            RAISE NOTICE 'Skipping notification for % because compressed payload is too large: % bytes', a_table_name, octet_length(compressed);
             RETURN;
         END IF;
 
-        PERFORM pg_notify('audit_channel', encode(compressed, 'base64'));
+        PERFORM pg_notify('audit_channel', compressed::text);
     ELSE
         PERFORM pg_notify('audit_channel', payload_text);
     END IF;
